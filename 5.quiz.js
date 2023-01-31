fb381aeb7048dd70a3dbc4adb42e5db603c67ff2
박상훈 <psh995666@gmail.com>
2023-01-31 10:00:52 +0900

    array quiz

--- 5.quiz.js
+++ 5.quiz.js
@@ -2,19 +2,49 @@
 // 단, 주어진 배열을 수정하지 않도록!
 // input: ['🍌', '🍓', '🍇', '🍓']
 // output: [ '🍌', '🥝', '🍇', '🥝' ]
-const fruits1 = ['🍌', '🍓', '🍇', '🍓'];
-function change() {
-  console.log('테스트완료');
+function replace(array, from, to) {
+  const replaced = Array.from(array);
+
+  for (let i = 0; i < replaced.length; i++) {
+    if (replaced[i] === from) {
+      // 교체는 index에 접근해도 가능, 추가삭제는 함수를 권고
+      replaced[i] = to;
+      // newArr.splice(i, 1, to);
+    }
+  }
+  return replaced;
 }
-change();
+const array = ['🍌', '🍓', '🍇', '🍓'];
+const result = replace(array, '🍓', '🥝');
+console.log(array, result);
 
 // 퀴즈2:
 // 배열과 특정한 요소를 전달받아,
 // 배열안에 그 요소가 몇개나 있는지 카운트 하는 함수 만들기
 // input: [ '🍌', '🥝', '🍇', '🥝' ], '🥝'
 // output: 2
+function count(array, item) {
+  let counter = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      counter++;
+    }
+  }
+  return counter;
+}
+console.log(count(['🍌', '🥝', '🍇', '🥝'], '🥝'));
 
 // 퀴즈3: 배열1, 배열2 두개의 배열을 전달받아,
 // 배열1 아이템중 배열2에 존재하는 아이템만 담고 있는 배열 반환
 // input: ['🍌', '🥝', '🍇'],  ['🍌', '🍓', '🍇', '🍓']
 // output: [ '🍌', '🍇' ]
+function match(input, search) {
+  const result = [];
+  for (let i = 0; i < input.length; i++) {
+    if (search.includes(input[i])) {
+      result.push(input[i]);
+    }
+  }
+  return result;
+}
+console.log(match(['🍌', '🥝', '🍇'], ['🍌', '🍓', '🍇', '🍓']));
